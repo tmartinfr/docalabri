232dbf0de5e242fc8727b5d1b034c60def0c4a0d
Swann Croiset <swann@oopss.org>
2016-04-23 23:34:11 +0200

    Fix settings.py
    local_settings.py is optional.

--- papierbackup/settings.py
+++ papierbackup/settings.py
@@ -120,7 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads'),
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/documents/'
 
-from local_settings import *
+try:
+    from local_settings import *
+except ImportError:
+    pass
