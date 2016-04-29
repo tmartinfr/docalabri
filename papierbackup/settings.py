853e0a4ce2aaddc3abff1603b01c345314edad72
Thomas Martin <thomas@oopss.org>
2016-04-29 09:02:04 +0200

    Fix template loading

--- papierbackup/settings.py
+++ papierbackup/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'papierbackup',
     'documents.apps.DocumentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'papierbackup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
