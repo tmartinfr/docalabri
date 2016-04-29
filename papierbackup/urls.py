a9b37579456366f4eecbef82d327486fdf4eba64
Thomas Martin <thomas@oopss.org>
2016-04-29 13:02:00 +0200

    Create custom view for index page
    IndexView

--- papierbackup/urls.py
+++ papierbackup/urls.py
@@ -17,7 +17,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
+from papierbackup.views import IndexView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='papierbackup_index.html'))
+    url(r'^$', IndexView.as_view(), name='index'),
 ]
