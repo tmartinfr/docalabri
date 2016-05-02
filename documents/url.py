564fe302a56214daa1469705c6ef3dfc4428ead2
Thomas Martin <thomas@oopss.org>
2016-05-02 21:04:13 +0200

    Allow to display document detail

--- documents/url.py
+++ documents/url.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from documents.views import DocumentListView
+from documents.views import DocumentListView, DocumentDetailView
 
 urlpatterns = [
     url(r'^$', DocumentListView.as_view(), name='document-list'),
+    url(r'^(?P<pk>[0-9]+)$', DocumentDetailView.as_view(), name='document-detail'),
 ]
--- documents/views.py
+++ documents/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from documents.models import Document
 
 class DocumentListView(ListView):
+    model = Document
+
+class DocumentDetailView(DetailView):
     model = Document
