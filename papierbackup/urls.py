a657f6bcb58796f4b2622846cf3b956b7f2f5229
Thomas Martin <thomas@oopss.org>
2016-05-02 21:18:39 +0200

    Change login template location

--- papierbackup/urls.py
+++ papierbackup/urls.py
@@ -23,7 +23,8 @@ from papierbackup.forms import CustomAuthenticationForm
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^login', auth_views.login, {'authentication_form': CustomAuthenticationForm}, name='login'),
+    url(r'^login', auth_views.login,
+        {'authentication_form': CustomAuthenticationForm, 'template_name': 'papierbackup/login.html'}, name='login'),
     url(r'^logout', auth_views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'^documents/', include('documents.url')),
 ]
