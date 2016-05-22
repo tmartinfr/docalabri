0ca7d2eaafc78c55922f60b533bdd07a9e53afe1
Thomas Martin <thomas@oopss.org>
2016-05-22 08:52:27 +0200

    Set error message when duplicate Contact's email

--- papierbackup/models.py
+++ papierbackup/models.py
@@ -1,8 +1,10 @@
+# coding: utf8
+
 from django.db import models
 
 
 class Contact(models.Model):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, error_messages={'unique': 'Cette adresse e-mail est déjà enregistrée'})
     creation_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
