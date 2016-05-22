a66939368f7952e3d955b804556572d1627a604c
Thomas Martin <thomas@oopss.org>
2016-05-22 08:23:31 +0200

    Add creation_date to Contact

--- papierbackup/models.py
+++ papierbackup/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Contact(models.Model):
     email = models.EmailField(unique=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.email
