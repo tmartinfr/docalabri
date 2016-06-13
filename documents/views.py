0fecece4ca944da306be85fe771f3d243705455f
Thomas Martin <thomas@oopss.org>
2016-06-13 13:28:00 +0200

    Refactor FileBaseDownload
    * Retrieve file using only one SQL request
    * Return HttpResponseForbidden() if file access not allowed

--- documents/views.py
+++ documents/views.py
@@ -1,11 +1,9 @@
-import os.path
 import mimetypes
 
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import PermissionDenied
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse, HttpResponseForbidden
 from django.utils.text import slugify
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -32,32 +30,25 @@ class DocumentDetailView(LoginRequiredMixin, DetailView):
         return Document.objects.filter(user=self.request.user)
 
 
-class FileBaseDownload(LoginRequiredMixin, View):
+class FileBaseDownload(View):
     def _get(self, request, doc_id, file_id, slug, download=True):
-        doc = Document.objects.get(user=self.request.user, pk=doc_id)
-        if not doc:
-            logger.warning(
-                "Unauthorized acces by user {} to [doc:%s]".format(
-                    self.request.user, doc)
-            )
-            raise PermissionDenied()
+        if not request.user.is_authenticated():
+            return HttpResponseForbidden()
 
-        f = File.objects.get(pk=file_id, document=doc)
-        if not f:
-            logger.warning(
-                "Unauthorized acces by user {} to [doc:%s][file:%s]".format(
-                    self.request.user, doc, f)
-            )
-            raise PermissionDenied()
+        try:
+            f = File.objects.select_related('document').get(pk=file_id, document__user=request.user)
+        except ObjectDoesNotExist:
+            logger.warning("Unauthorized access by user {} to [file:%s]".format(request.user, f))
+            return HttpResponseForbidden()
 
-        logger.debug("Accessing [doc:%s][file:%s]" % (doc, f))
+        logger.debug("Accessing [file:%s]" % (f))
         response = HttpResponse()
         mimetype = mimetypes.guess_type(f.file.url)
         response['Content-Type'] = mimetype[0]
         if download:
             name = f.file.url.split('/')[-1]
             response['Content-Disposition'] = \
-                'attachment; filename={}-{}'.format(slugify(doc), name)
+                'attachment; filename={}-{}'.format(slugify(f.document), name)
 
         redir = f.file.url.replace(settings.MEDIA_URL, settings.PRIVATE_URL, 1)
         response['X-Accel-Redirect'] = redir
