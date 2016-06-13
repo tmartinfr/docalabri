528e4e4353c39f2afab7b7edef111377d4aede3c
Thomas Martin <thomas@oopss.org>
2016-06-13 11:50:28 +0200

    Add correct namespace to view names

--- documents/url.py
+++ documents/url.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', DocumentListView.as_view(), name='document-list'),
     url(r'^(?P<pk>[0-9]+)/?$', DocumentDetailView.as_view(), name='document-detail'),
     url(r'^download/(?P<doc_id>\d+)/(?P<file_id>\d+)/(?P<slug>.+)$',
-        FileDownload.as_view(), name='download'),
+        FileDownload.as_view(), name='document-download'),
     url(r'^preview/(?P<doc_id>\d+)/(?P<file_id>\d+)/(?P<slug>.+)$',
-        FilePreview.as_view(), name='preview'),
+        FilePreview.as_view(), name='document-preview'),
 ]
